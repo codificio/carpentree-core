1252f8901fe3dd8e45e5788807a68ed6dcb3ddf0
Enrico Nardo <enr.nardo@gmail.com>
2019-02-19 15:19:01 +0100

    Update views and root route

--- src/Providers/CoreServiceProvider.php
+++ src/Providers/CoreServiceProvider.php
@@ -32,9 +32,7 @@ class CoreServiceProvider extends ServiceProvider
 
         $this->mapRoutes();
 
-        $this->publishConfig();
-
-        $this->publishResources();
+        $this->publish();
 
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
@@ -42,7 +40,7 @@ class CoreServiceProvider extends ServiceProvider
         }
 
         // Views
-        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'carpentree');
+        // $this->loadViewsFrom(__DIR__.'/../../resources/views', 'carpentree');
 
         // Middlewares
         /** @var Router $router */
@@ -63,22 +61,30 @@ class CoreServiceProvider extends ServiceProvider
         );
     }
 
-    public function publishResources()
+    public function publish()
     {
+        // Assets
         $this->publishes([
-            __DIR__ . '/../../resources/assets' => resource_path('assets/vendor/carpentree/core')
+            __DIR__ . '/../../resources/assets' => resource_path('vendor/carpentree/core')
         ], 'assets');
-    }
 
-    public function publishConfig()
-    {
+        // Public assets
+        /*
+        $this->publishes([
+            __DIR__ . '/../../resources/public' => public_path('vendor/carpentree/core')
+        ], 'public');
+        */
+
+        // Config
         $this->publishes([
             __DIR__.'/../../config/core.php' => config_path('carpentree/core.php'),
-        ], 'config');
-
-        $this->publishes([
             __DIR__.'/../../config/permissions.php' => config_path('carpentree/permissions.php'),
         ], 'config');
+
+        // Migrations
+        $this->publishes([
+            __DIR__.'/../../database/migrations/' => database_path('migrations')
+        ], 'migrations');
     }
 
     /**
@@ -91,7 +97,7 @@ class CoreServiceProvider extends ServiceProvider
     protected function mapRoutes()
     {
         $this->loadRoutesFrom(__DIR__.'/../../routes/api/admin.php');
-        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+        // $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
         $this->loadRoutesFrom(__DIR__.'/../../routes/auth.php');
     }
 
