a628be3a816dc961dede4e3af4a41e641827b88f
Enrico Nardo <enr.nardo@gmail.com>
2019-03-13 14:18:08 +0100

    getMetaByKey method for HasMeta Trait

--- src/Traits/HasMeta.php
+++ src/Traits/HasMeta.php
@@ -4,12 +4,28 @@ namespace Carpentree\Core\Traits;
 
 use Carpentree\Core\Models\MetaField;
 use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 trait HasMeta
 {
     public function meta()
     {
         return $this->morphMany($this->getMetaModelClassName(), 'model', 'model_type','model_id');
+    }
+
+    public function getMetaByKey($key)
+    {
+        $meta = $this->meta()->where('key', $key);
+
+        if (!$meta) {
+            throw new NotFoundHttpException(__("Meta field with key :key was not found for model :model with id :id", [
+                'key' => $key,
+                'model' => get_class($this),
+                'id' => $this->id
+            ]));
+        }
+
+        return $meta;
     }
 
     public function syncMeta(array $meta)
