651b419f4f212fc988e96cfd37f2396ab2094dd3
Enrico Nardo <enr.nardo@gmail.com>
2019-03-13 15:52:00 +0100

    New methods for HasMeta trait

--- src/Traits/HasMeta.php
+++ src/Traits/HasMeta.php
@@ -5,17 +5,25 @@ namespace Carpentree\Core\Traits;
 use Carpentree\Core\Models\MetaField;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Exception;
 
 trait HasMeta
 {
+    /**
+     * @return mixed
+     */
     public function meta()
     {
         return $this->morphMany($this->getMetaModelClassName(), 'model', 'model_type','model_id');
     }
 
-    public function getMetaByKey($key)
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function getMetaByKeyOrFail($key)
     {
-        $meta = $this->meta()->where('key', $key)->first();
+        $meta = $this->getMetaByKey($key);
 
         if (!$meta) {
             throw new NotFoundHttpException(__("Meta field with key :key was not found for model :model with id :id", [
@@ -28,6 +36,44 @@ trait HasMeta
         return $meta;
     }
 
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function getMetaByKey($key)
+    {
+        $meta = $this->meta()->where('key', $key)->first();
+
+        return $meta ?? false;
+    }
+
+
+    /**
+     * @param array $meta
+     * @return mixed
+     * @throws Exception
+     */
+    public function createOrUpdateMeta(array $meta)
+    {
+        if (!array_key_exists('key', $meta) || !array_key_exists('value', $meta)) {
+            throw new Exception(__("Missing key or value of the meta fields for model of id :id", ['id' => $this->id]));
+        }
+
+        $_meta = $this->getMetaByKey($meta['key']);
+
+        if ($_meta) {
+            $_meta->value = $meta['value'];
+        } else {
+            $_meta = new MetaField($meta);
+        }
+
+        return $this->meta()->save($_meta);
+    }
+
+    /**
+     * @param array $meta
+     * @return $this
+     */
     public function syncMeta(array $meta)
     {
         // Works only with array like:
@@ -74,6 +120,9 @@ trait HasMeta
         return $this;
     }
 
+    /**
+     * @return string
+     */
     protected function getMetaModelClassName(): string
     {
         return MetaField::class;
