b324ae87911deef2068bd2807eed8ed0d58e079d
Enrico Nardo <enr.nardo@gmail.com>
2019-03-22 11:30:41 +0100

    Update Eloquent Base Data Access

--- src/DataAccess/Eloquent/EloquentBaseDataAccess.php
+++ src/DataAccess/Eloquent/EloquentBaseDataAccess.php
@@ -3,6 +3,7 @@
 namespace Carpentree\Core\DataAccess\Eloquent;
 
 use Carpentree\Core\DataAccess\BaseDataAccess;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Exception;
 
@@ -20,19 +21,59 @@ class EloquentBaseDataAccess implements BaseDataAccess
         $this->class = $class;
     }
 
+    /**
+     * @param $id
+     * @return Model
+     */
     public function find($id)
     {
         return $this->class::find($id);
     }
 
+    /**
+     * @param $id
+     * @return Model
+     */
     public function findOrFail($id)
     {
         return $this->class::findOrFail($id);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function all()
     {
         return $this->class::all();
+    }
+
+    /**
+     * @param Builder $builder
+     * @return Builder[]|\Illuminate\Database\Eloquent\Collection
+     */
+    protected function get(Builder $builder)
+    {
+        return $builder->get();
+    }
+
+    /**
+     * @param Builder $builder
+     * @return Builder|Model|object|null
+     */
+    protected function first(Builder $builder)
+    {
+        return $builder->first();
+    }
+
+    /**
+     * @param Builder $builder
+     * @param null $perPage
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    protected function paginate(Builder $builder, $perPage = null)
+    {
+        $perPage = $perPage ?: config('carpentree.core.pagination.per_page');
+        return $builder->paginate($perPage);
     }
 
     /**
